9578119923b9dc28ee0429321ca5b0db62740cb9
misken <isken@oakland.edu>
2019-01-24 12:07:26 -0500

    working on multiple grouping fields

--- bikeshare_util.R
+++ bikeshare_util.R
@@ -10,7 +10,7 @@
 # Do I want to be able to call this dplyr style (unquoted field names) or just use field name strings?
 #   Let's start by designing to be used dplyr style
 
-counts_bydate <- function(df, start, end, date_var, group_var){
+counts_by_date_group <- function(df, start, end, date_var, group_var){
   
   # Use enquo to enable dplyr style calling
   date_var <- enquo(date_var)
@@ -20,9 +20,6 @@ counts_bydate <- function(df, start, end, date_var, group_var){
   group_var_name <- quo_name(group_var)
   date_var_name <- quo_name(date_var)
   
-  # Could add this to function as a parameter
-  datefld_name <- "date"
-  
   dates <- seq(start, end, by='days')
   
   # The following results in a one column tibble. Yes, even with as.vector().
@@ -31,6 +28,51 @@ counts_bydate <- function(df, start, end, date_var, group_var){
   grp <- df %>% distinct(!!group_var) %>% pull()
   # Or this using unlist
   # grp <- unlist(unique(df[, group_var]), use.names = FALSE)
+  
+  # https://tidyr.tidyverse.org/reference/expand.html
+  # Use expand.grid() to create the fully seeded dataframe
+  date_grp <- expand.grid(grp, dates)
+  names(date_grp) <- c(group_var_name, date_var_name)
+  
+  # Get sorted by station and date
+  date_grp <- date_grp %>% 
+    arrange(!!group_var, !!date_var)
+  
+  # Count trips out
+  counts_group_date <- trip %>% 
+    filter(between(!!date_var, startdate, enddate)) %>% 
+    group_by(!!group_var, !!date_var) %>% 
+    count()
+  
+  # Join the fully seeded dataframe to the count dataframe 
+  counts_group_date <- date_grp %>% 
+    left_join(counts_group_date, by = c(group_var_name, date_var_name))
+  
+  # Replace the NAs with 0's.
+  counts_group_date$n <- counts_group_date$n %>% replace_na(0)
+  
+  counts_group_date
+  
+}
+
+# Extend to handle multiple grouping fields
+counts_by_date_groups <- function(df, start, end, date_var, ...){
+  
+  # Use enquo to enable dplyr style calling
+  date_var <- enquo(date_var)
+  group_var <- group_var <- quos(...)
+
+  
+  # Need to use quo_name() to convert group_var quosure to string to use as new column name
+  group_var_name <- quo_name(group_var)
+  date_var_name <- quo_name(date_var)
+  
+  dates <- seq(start, end, by='days')
+  
+  # Find distinct combinations of the grouping variables
+  grps <- df %>% distinct(!!group_var) 
+  
+  # Append the dates sequenc
   
   # Use expand.grid() to create the fully seeded dataframe
   date_grp <- expand.grid(grp, dates)
